abd1e186070a6e3549c9a7def83cf03803564898
Dan Tong <danieltongnz@gmail.com>
2016-01-25 10:02:45 +1300

    Add conditional logic and redirects depending on whether question has been answered

--- app/views.py
+++ app/views.py
@@ -1,16 +1,29 @@
-from flask import render_template, g
+from flask import render_template, g, url_for, request, redirect
 from app import app, db
 from .models import User, Answer
 from .forms import QuestionForm
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index')
-def index():
-    form = QuestionForm()
 
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    # Check if question has been answered, if so, show results page
+    answered = request.cookies.get('answered')
+    if answered == 'yes':
+        return redirect(url_for('results'))
+
+    form = QuestionForm()
     if form.validate_on_submit():
         print(form.options.data)
+        response = redirect(url_for('results'))
+        response.set_cookie('answered', 'yes')
+        return response
     else:
         print(form.errors)
 
     return render_template('index.html', form=form)
+
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Future Enhancements
+Use something better than cookies to identify whether user has answered question. 
