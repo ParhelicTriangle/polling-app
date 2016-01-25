2379bdbf297c7181c675d812637d466a9b96de8d
Dan Tong <danieltongnz@gmail.com>
2016-01-25 17:54:31 +1300

    Pull form values from database

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,12 @@
 from flask.ext.wtf import Form
 from wtforms import SelectField
+from .models import Answer
 
 class QuestionForm(Form):
-    options = SelectField('Label', choices=[
-                                            ('apples','apples'),
-                                            ('bananas', 'bananas'),
-                                            ('oranges', 'oranges')
-                                            ])
+    choices = []
+    answers = Answer.query.all()
+    for answer in answers:
+        answer = (answer.id, answer.name)
+        choices.append(answer)
+        
+    options = SelectField('Label', choices=choices)
